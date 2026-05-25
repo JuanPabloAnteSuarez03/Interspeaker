e6c32379bf953c665c9e64907f4dad83a34e2bcd
SARTDv <munozalejandro531@gmail.com>
2026-05-25 15:25:40 -0500

    configuracion inicial storage

---
+++ Backend/config/storage_clients.py
@@ -0,0 +1,18 @@
+import os
+import boto3
+from google.cloud import firestore
+from botocore.client import Config
+
+
+db = firestore.Client()
+
+s3_client = boto3.client(
+    "s3",
+    endpoint_url=os.getenv("STORAGE_ENDPOINT"),
+    aws_access_key_id=os.getenv("STORAGE_ROOT_USER"),
+    aws_secret_access_key=os.getenv("STORAGE_ROOT_PASSWORD"),
+    config=Config(signature_version="s3v4"),
+    region_name="us-east-1"
+)
+
+BUCKET_NAME = os.getenv("STORAGE_BUCKET_NAME")
